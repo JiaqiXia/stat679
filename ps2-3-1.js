745164028d2f915661d1c45fbc72496a8229e9e1
JiaqiXia <98632376+JiaqiXia@users.noreply.github.com>
2022-10-21 20:30:01 -0500

    Update ps2-3-1.js

--- ps2-3-1.js
+++ ps2-3-1.js
@@ -1,15 +1,23 @@
 
 // generates a random set of 10 numbers
-let generator = d3.random();
+let generator = d3.randomUniform();
 let x = generator(10);
+let center = [];
+for (let i = 0; i < 10; i++) {
+    center.push(generator(10))
+  }
+let center2 = [];
+for (let i = 0; i < 10; i++) {
+    center2.push(generator(10))
+    }
 
 d3.select("svg")
   .selectAll("circle")
   .data(center).enter()
   .append("circle")
   .attrs({
-    r: 3,
-    cx: d => d * 200,
-    cy: 200,
+    r: 5,
+    cx: d => d * 400,
+    cy: 100,
     fill: "black"
   })
